d353bcf9bbb4714380e54a3617aa9209230263ac
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-09-01 14:30:26 +0900

    funcDeclからオフセット情報を取れるようになった

--- gentest.go
+++ gentest.go
@@ -3,6 +3,7 @@ package gentest
 import (
 	"fmt"
 	"go/ast"
+	"go/token"
 	"io"
 	"os"
 	"strings"
@@ -55,10 +56,32 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	of.TestFuncName = genTestFuncName(funcName)
 	outputTestCode(of)
 
+	_, _ = findTargetFunc(pass)
+
 	return nil, nil
 }
 
 func findTargetFunc(pass *analysis.Pass) (*ast.FuncDecl, error) {
+	for _, file := range pass.Files {
+		fmt.Printf("filename: %s\n", file.Name.String())
+		token.NewFileSet()
+
+		for _, decl := range file.Decls {
+			funcDecl, _ := decl.(*ast.FuncDecl)
+			if funcDecl == nil {
+				continue
+			}
+
+			fmt.Println(funcDecl.Name.String())
+
+			blockStmt := funcDecl.Body
+			lbrecePosition := pass.Fset.Position(blockStmt.Lbrace)
+			fmt.Printf("lbrace: %d, line: %d\n", lbrecePosition.Offset, lbrecePosition.Line)
+			rbrecePosition := pass.Fset.Position(blockStmt.Rbrace)
+			fmt.Printf("rbrace: %d, line: %d\n", rbrecePosition.Offset, rbrecePosition.Line)
+
+		}
+	}
 	return nil, nil
 }
 
